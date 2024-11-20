66376a67989c7cd44477b114e4b34d2f8d15277d
varadaraju17 <varada883@gmail.com>
2024-11-20 13:22:00 +0530

    Intiating thr Project

---
+++ src/main/java/com/springboot/pharmaassit/PharmaAssistApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.pharmaassit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/pharmaassit/PharmaAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.pharmaassit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
